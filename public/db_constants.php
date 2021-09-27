41bca953f6813a7010c0c766c42574dd7dd6b9d0
titikdua <titikdua35@gmail.com>
2021-09-27 13:55:20 +0700

    edit connection for response error

--- public/db_constants.php
+++ public/db_constants.php
@@ -1,7 +1,7 @@
 <?php
 	           defined('CAMHRMS_HOST') || define('CAMHRMS_HOST','localhost');
 	           defined('CAMHRMS_USERNAME') || define('CAMHRMS_USERNAME','root');
-	           defined('CAMHRMS_PASSWORD') || define('CAMHRMS_PASSWORD','Allifa12#');
+	           defined('CAMHRMS_PASSWORD') || define('CAMHRMS_PASSWORD','');
 	           defined('CAMHRMS_DBNAME') || define('CAMHRMS_DBNAME','mgs_hr');
 	           
 	         ?>
