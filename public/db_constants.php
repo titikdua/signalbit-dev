2dd819ba56b6d9ae8acfd8d0edb212c6390626a1
titikdua <titikdua35@gmail.com>
2021-09-30 18:15:39 +0700

    edit connection

--- public/db_constants.php
+++ public/db_constants.php
@@ -1,5 +1,5 @@
 <?php
-	           defined('CAMHRMS_HOST') || define('CAMHRMS_HOST','192.168.10.19');
+	           defined('CAMHRMS_HOST') || define('CAMHRMS_HOST','192.168.98.192');
 	           defined('CAMHRMS_USERNAME') || define('CAMHRMS_USERNAME','root');
 	           defined('CAMHRMS_PASSWORD') || define('CAMHRMS_PASSWORD','Mgs@2021');
 	           defined('CAMHRMS_DBNAME') || define('CAMHRMS_DBNAME','signalbit_hr');
