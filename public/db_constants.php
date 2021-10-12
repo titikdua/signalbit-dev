0780728e8c6e1439b5d149bdb9aa9f6a7dd5ff7d
titikdua <titikdua35@gmail.com>
2021-10-12 15:00:10 +0700

    sadasfasf

--- public/db_constants.php
+++ public/db_constants.php
@@ -2,7 +2,7 @@
 	           defined('CAMHRMS_HOST') || define('CAMHRMS_HOST','192.168.98.192');
 	           defined('CAMHRMS_USERNAME') || define('CAMHRMS_USERNAME','root');
 	           defined('CAMHRMS_PASSWORD') || define('CAMHRMS_PASSWORD','Mgs@2021'); 
-	           defined('CAMHRMS_DBNAME') || define('CAMHRMS_DBNAME','signalbit_hr'); 	////abc
+	           defined('CAMHRMS_DBNAME') || define('CAMHRMS_DBNAME','signalbit_hr'); 	
 	           
 	         ?>
 
