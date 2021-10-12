5ca2a952d7a1e29066b438813a15d1306cc01f65
titikdua <titikdua35@gmail.com>
2021-10-12 10:31:19 +0700

    test jenkins

--- public/db_constants.php
+++ public/db_constants.php
@@ -1,7 +1,7 @@
 <?php
 	           defined('CAMHRMS_HOST') || define('CAMHRMS_HOST','192.168.98.192');
 	           defined('CAMHRMS_USERNAME') || define('CAMHRMS_USERNAME','root');
-	           defined('CAMHRMS_PASSWORD') || define('CAMHRMS_PASSWORD',''); //Mgs@2021
+	           defined('CAMHRMS_PASSWORD') || define('CAMHRMS_PASSWORD',''); //Mgs@21
 	           defined('CAMHRMS_DBNAME') || define('CAMHRMS_DBNAME','signalbit_hr');
 	           
 	         ?>
