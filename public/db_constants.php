f5c38824cda78b3a9f11a1cb57840905c519e16f
titikdua <titikdua35@gmail.com>
2021-10-12 16:31:57 +0700

    testing

--- public/db_constants.php
+++ public/db_constants.php
@@ -2,7 +2,7 @@
 	           defined('CAMHRMS_HOST') || define('CAMHRMS_HOST','192.168.98.192');
 	           defined('CAMHRMS_USERNAME') || define('CAMHRMS_USERNAME','root');
 	           defined('CAMHRMS_PASSWORD') || define('CAMHRMS_PASSWORD','Mgs@2021'); 
-	           defined('CAMHRMS_DBNAME') || define('CAMHRMS_DBNAME','signalbit_hr'); 	
+	           defined('CAMHRMS_DBNAME') || define('CAMHRMS_DBNAME','signalbit_hr'); 	//ini adalah testing ocp
 	           
 	         ?>
 
